90b293216a32f39522a4db0af71bc501f3d73257
ciri-ryan <cirijaye@yahoo.com>
2022-07-26 11:41:33 -0700

    useEffect  to check win or tie set player check winner reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Square from './components/Square';
+import { Patterns } from './Patterns';
 
 function App() {
   const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
-  const [player, setPlayer] = useState('X');
+  const [player, setPlayer] = useState('O');
+  const [result, setResult] = useState({winner: 'none', state: 'none'});
+
+
+  useEffect(() => {
+    checkWin();
+    checkTie();
+  
+    if(player === 'X') {
+      setPlayer('O')
+    } else {
+      setPlayer('X')
+    }
+    
+  },[board])
+
+  useEffect(() => {
+    if(result.state !== 'none'){
+      alert(`Game Finished! winner: ${result.winner}`)
+      reset();
+    }
+  },[result])
 
   const chooseSquare = (square) => {
     setBoard(
@@ -15,12 +37,43 @@ function App() {
       return val;
     })
     );
-    if(player === 'X') {
-      setPlayer('O')
-    } else {
-      setPlayer('X')
-    }
+   
   };
+
+  const checkWin = () => {
+    Patterns.forEach((currPattern) => {
+      const player1 = board[currPattern[0]]
+      if (player1 === '') return;
+      let foundWinPattern = true;
+      currPattern.forEach(idx => {
+        if(board[idx] != player1){
+          foundWinPattern = false;
+        }
+      });
+
+      if(foundWinPattern) {
+        setResult({ winner: player, state: 'Won' })
+      }
+    })
+  }
+
+  const checkTie = () => {
+    let filled = true;
+    board.forEach(square => {
+      if(square === '') {
+        filled = false;
+      }
+    })
+    if(filled) {
+      setResult({ winner: 'No one', state: 'Tie Game' })
+    }
+  }
+
+  const reset = () => {
+    setBoard(['', '', '', '', '', '', '', '', ''])
+    setResult({winner: 'none', state: 'none'})
+    setPlayer('O')
+  }
 
   return (
     <div className="App">
